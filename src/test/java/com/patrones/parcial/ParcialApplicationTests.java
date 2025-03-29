74629b1eea3b9ccbaf8baacd7da54e262559ea8b
sergi <sergio_socha@yahoo.com>
2025-03-29 18:42:02 -0500

    Agrega proyecto spring

---
+++ src/test/java/com/patrones/parcial/ParcialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.patrones.parcial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParcialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
